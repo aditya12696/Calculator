df25a19fac93ebdd0ba2381bc5e3057c89589a5b
aditya12696 <100424719+aditya12696@users.noreply.github.com>
2025-01-07 12:17:54 +0530

    3 commit

--- calculator.java
+++ calculator.java
@@ -1,11 +1,14 @@
 class calculator{
     public static void main(String[] args) {
-        // first code 
+        // 1 code 
         System.out.println("this is calculator");
     }
-    // second code
+    // 2 code
     public static int add(int a, int b){
         return a+b;
     }
-    
+    // 3 code
+    public static int sub(int a, int b){
+        return a-b;
+    }
 }
